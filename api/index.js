4b2b9dc5cb3d3928dda102d955b01c0c9bffeebd
josedhonatas <dhonatas@academico.ufs.br>
2023-04-17 14:28:23 -0300

    negocio-ali

--- api/index.js
+++ api/index.js
@@ -9,9 +9,9 @@ const autorRoutes = require('./routes/autor')(pool);
 const revisorRoutes = require('./routes/revisor')(pool);
 const gestorRoutes = require('./routes/gestor')(pool);
 const administradorRoutes = require('./routes/administrador')(pool);
-
-
-
+const artigoRoutes = require('./routes/artigo')(pool);
+const avaliacaoRoutes = require('./routes/avaliacao')(pool);
+const parecerRoutes = require('./routes/parecer')(pool);
 
 
 app.use('/usuarios', usuariosRoutes);
@@ -20,8 +20,10 @@ app.use('/editor', editorRoutes);
 app.use('/autor', autorRoutes);
 app.use('/revisor', revisorRoutes);
 app.use('/gestor', gestorRoutes);
-
-
+app.use('/administrador', administradorRoutes);
+app.use('/artigo', artigoRoutes);
+app.use('/avaliacao', avaliacaoRoutes);
+app.use('/parecer', parecerRoutes);
 
 
 app.listen(porta, () => {
