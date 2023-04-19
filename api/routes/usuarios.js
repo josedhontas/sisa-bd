94b2197a1a4a8c11217bd4883576effd2f480413
josedhonatas <dhonatas@academico.ufs.br>
2023-04-19 13:52:08 -0300

    mudancas-rotas

--- api/routes/usuarios.js
+++ api/routes/usuarios.js
@@ -2,61 +2,81 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (pool) => {
-  router.get('/', async (req, res) => {
+  // Rota para buscar todos os usuários
+  router.get('/usuarios', async (req, res) => {
     try {
-      const { rows } = await pool.query('SELECT * FROM usuario');
-      res.json(rows);
+      const result = await pool.query('SELECT * FROM usuario');
+      res.json(result.rows);
     } catch (error) {
-      res.status(500).json({ error: error.message });
+      console.error(error);
+      res.status(500).send('Erro ao buscar usuários');
     }
   });
 
-  router.get('/:email', async (req, res) => {
+  // Rota para buscar um usuário por email
+  router.get('/usuarios/:email', async (req, res) => {
+    const { email } = req.params;
+    try {
+      const result = await pool.query('SELECT * FROM usuario WHERE email = $1', [email]);
+      if (result.rows.length === 0) {
+        res.status(404).send('Usuário não encontrado');
+      } else {
+        res.json(result.rows[0]);
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Erro ao buscar usuário');
+    }
+  });
+
+  // Rota para criar um usuário
+  router.post('/usuarios', async (req, res) => {
+    const { email, nome_completo, senha, telefone, departamento, descricao, link_imagem } = req.body;
+    try {
+      const result = await pool.query(
+        'INSERT INTO usuario (email, nome_completo, senha, telefone, departamento, descricao, link_imagem) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
+        [email, nome_completo, senha, telefone, departamento, descricao, link_imagem]
+      );
+      res.status(201).json(result.rows[0]);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Erro ao criar usuário');
+    }
+  });
+
+  router.put('/usuario/:email', (req, res) => {
     const email = req.params.email;
-    try {
-      const { rows } = await pool.query('SELECT * FROM usuario WHERE email = $1', [email]);
-      res.json(rows);
-    } catch (error) {
-      res.status(500).json({ error: 'usuario nao encontrado' });
-    }
+    const { nome_completo, senha, telefone, departamento, descricao, link_imagem } = req.body;
+    const query = 'UPDATE usuario SET nome_completo=$1, senha=$2, telefone=$3, departamento=$4, descricao=$5, link_imagem=$6 WHERE email=$7';
+  
+    pool.query(query, [nome_completo, senha, telefone, departamento, descricao, link_imagem, email], (error, results) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send('Erro ao atualizar usuário.');
+      } else {
+        res.status(200).send(`Usuário ${email} atualizado com sucesso.`);
+      }
+    });
   });
+  
 
-  router.post('/', async (req, res) => {
-    const { email, nome_completo, senha, telefone, departamento, descricao } = req.body;
+  router.delete('/usuario/:email', (req, res) => {
+    const email = req.params.email;
+    const query = 'DELETE FROM usuario WHERE email=$1';
   
-    try {
-      const { rows } = await pool.query('INSERT INTO usuario (email, nome_completo, senha, telefone, departamento, descricao) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', [email, nome_completo, senha, telefone, departamento, descricao]);
-      res.json(rows[0]);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
+    pool.query(query, [email], (error, results) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send('Erro ao excluir usuário.');
+      } else {
+        res.status(200).send(`Usuário ${email} excluído com sucesso.`);
+      }
+    });
   });
+  
 
-  router.put('/:email', async (req, res) => {
-    const { email } = req.params;
-    const { nome_completo, senha, telefone, departamento, descricao } = req.body;
-  
-    try {
-      const { rows } = await pool.query('UPDATE usuario SET nome_completo=$1, senha=$2, telefone=$3, departamento=$4, descricao=$5 WHERE email=$6 RETURNING *', [nome_completo, senha, telefone, departamento, descricao, email]);
-      res.json(rows[0]);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  });
 
-  router.delete('/:email', async (req, res) => {
-    const { email } = req.params;
-  
-    try {
-      const { rows } = await pool.query('DELETE FROM usuario WHERE email=$1 RETURNING *', [email]);
-      res.json(rows[0]);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  });
-  
-  
-  
+
 
   return router;
 };
