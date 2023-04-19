7407f672db0f0018e059f25752c4774ae6ad0cd1
josedhonatas <dhonatas@academico.ufs.br>
2023-04-19 13:58:31 -0300

    correcao

--- api/routes/usuarios.js
+++ api/routes/usuarios.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 module.exports = (pool) => {
   // Rota para buscar todos os usuários
-  router.get('/usuarios', async (req, res) => {
+  router.get('/', async (req, res) => {
     try {
       const result = await pool.query('SELECT * FROM usuario');
       res.json(result.rows);
@@ -14,7 +14,7 @@ module.exports = (pool) => {
   });
 
   // Rota para buscar um usuário por email
-  router.get('/usuarios/:email', async (req, res) => {
+  router.get('/:email', async (req, res) => {
     const { email } = req.params;
     try {
       const result = await pool.query('SELECT * FROM usuario WHERE email = $1', [email]);
@@ -30,7 +30,7 @@ module.exports = (pool) => {
   });
 
   // Rota para criar um usuário
-  router.post('/usuarios', async (req, res) => {
+  router.post('/', async (req, res) => {
     const { email, nome_completo, senha, telefone, departamento, descricao, link_imagem } = req.body;
     try {
       const result = await pool.query(
@@ -44,7 +44,7 @@ module.exports = (pool) => {
     }
   });
 
-  router.put('/usuario/:email', (req, res) => {
+  router.put('/:email', (req, res) => {
     const email = req.params.email;
     const { nome_completo, senha, telefone, departamento, descricao, link_imagem } = req.body;
     const query = 'UPDATE usuario SET nome_completo=$1, senha=$2, telefone=$3, departamento=$4, descricao=$5, link_imagem=$6 WHERE email=$7';
@@ -60,7 +60,7 @@ module.exports = (pool) => {
   });
   
 
-  router.delete('/usuario/:email', (req, res) => {
+  router.delete('/:email', (req, res) => {
     const email = req.params.email;
     const query = 'DELETE FROM usuario WHERE email=$1';
   
