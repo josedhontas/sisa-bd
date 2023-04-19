d8e0fc02037b9dc7105bca736ea310af0d3d3cc1
josedhonatas <dhonatas@academico.ufs.br>
2023-04-19 14:08:39 -0300

    admin

--- api/routes/administrador.js
+++ api/routes/administrador.js
@@ -2,57 +2,55 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (pool) => {
-  router.get('/', async (req, res) => {
-    try {
-      const { rows } = await pool.query("SELECT usuario.email, usuario.nome,usuario.departamento papel.nome_papel FROM usuario JOIN papel ON usuario.email = papel.email WHERE papel.nome_papel = 'Administrador'"); res.json(rows);
-        res.json(rows);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  });
-
+  // Rota para buscar um administrador por email
   router.get('/:email', async (req, res) => {
-    const email = req.params.email;
     try {
-      const { rows } = await pool.query("SELECT usuario.email, usuario.nome_completo,usuario.departamento, papel.nome_papel FROM usuario JOIN papel ON usuario.email = papel.email where papel.nome_papel = 'Administrador' and usuario.email = $1", [email]);      res.json(rows);
+      const { email } = req.params;
+      const result = await pool.query('SELECT * FROM administrador WHERE email = $1', [email]);
+      res.status(200).json(result.rows[0]);
     } catch (error) {
-      res.status(500).json({ error: 'administrador nao encontrado' });
+      console.error(error);
+      res.status(500).json({ message: 'Erro ao buscar administrador por email' });
     }
   });
 
+  // Rota para criar um novo administrador
   router.post('/', async (req, res) => {
-    const {id_papel } = req.body;
-  
     try {
-      const { rows } = await pool.query('INSERT INTO administrador (id_papel) VALUES ($1) RETURNING *', [id_papel]);
-      res.json(rows[0]);
+      const { email} = req.body;
+      const result = await pool.query("INSERT INTO administrador (email, cargo) VALUES ($1, 'Administrador') RETURNING *", [email]);
+      res.status(201).json(result.rows[0]);
     } catch (error) {
-      res.status(500).json({ error: error.message });
+      console.error(error);
+      res.status(500).json({ message: 'Erro ao criar novo administrador' });
     }
   });
 
-  router.put('/:id', async (req, res) => {
-    const { id_administrador } = req.params;
-    const { id_papel} = req.body;
-  
+  // Rota para atualizar um administrador existente
+  router.put('/:email', async (req, res) => {
     try {
-      const { rows } = await pool.query('UPDATE administrador SET id_papel=$1 WHERE id_administrador=$2 RETURNING *', [id_papel, id_administrador]);
-      res.json(rows[0]);
+      const { email } = req.params;
+      const { cargo } = req.body;
+      const result = await pool.query('UPDATE administrador SET cargo = $1 WHERE email = $2 RETURNING *', [cargo, email]);
+      res.status(200).json(result.rows[0]);
     } catch (error) {
-      res.status(500).json({ error: error.message });
+      console.error(error);
+      res.status(500).json({ message: 'Erro ao atualizar administrador' });
     }
   });
 
-  router.delete('/:id', async (req, res) => {
-    const { id_administrador } = req.params;
-  
+  // Rota para deletar um administrador existente
+  router.delete('/:email', async (req, res) => {
     try {
-      const { rows } = await pool.query('DELETE FROM administrador WHERE id_administrador=$1 RETURNING *', [id_administrador]);
-      res.json(rows[0]);
+      const { email } = req.params;
+      await pool.query('DELETE FROM administrador WHERE email = $1', [email]);
+      res.status(204).send();
     } catch (error) {
-      res.status(500).json({ error: error.message });
+      console.error(error);
+      res.status(500).json({ message: 'Erro ao deletar administrador' });
     }
   });
-  
+
+
   return router;
 };
