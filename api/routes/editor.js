a3f65798667f87270b5522b3dd4361f4b0422df6
josedhonatas <dhonatas@academico.ufs.br>
2023-04-21 12:30:39 -0300

    rota_editor

--- api/routes/editor.js
+++ api/routes/editor.js
@@ -18,7 +18,7 @@ module.exports = (pool) => {
   router.get('/:email', async (req, res) => {
     try {
       const { email } = req.params;
-      const result = await pool.query('SELECT * FROM editor WHERE email = $1', [email]);
+      const result = await pool.query('select usuario.nome_completo, revista.nome_revista, revista.descricao from usuario inner join editor using(email) join trabalha_editor using(id_editor) join revista using(id_revista) WHERE email = $1', [email]);
       res.status(200).json(result.rows[0]);
     } catch (error) {
       console.error(error);
