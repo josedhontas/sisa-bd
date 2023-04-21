16f4857843d6918389d5e976e437b2653f23ee73
josedhonatas <dhonatas@academico.ufs.br>
2023-04-21 13:11:08 -0300

    revista_delete

--- api/routes/revista.js
+++ api/routes/revista.js
@@ -2,6 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (pool) => {
+  router.delete('/id:', async (req, res) => {
+    try {
+      const { id } = req.params;
+      await pool.query('DELETE FROM revista WHERE id_revista = $1', [id]);
+      res.status(204).send();
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Erro ao deletar revista' });
+    }
+  });
   
   return router;
 };
