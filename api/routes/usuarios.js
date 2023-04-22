a812ac395ac52caac30b7561e00043013a8b4744
josedhonatas <dhonatas@academico.ufs.br>
2023-04-22 20:28:16 -0300

    teste_middleware

--- api/routes/usuarios.js
+++ api/routes/usuarios.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+const authenticate = (req, res, next) => {
+  const isAuthenticated = true;
+
+  if (isAuthenticated) {
+    next();
+  } else {
+    res.status(401).send('Não autorizado');
+  }
+};
+
 module.exports = (pool) => {
   // Rota para buscar todos os usuários
-  router.get('/', async (req, res) => {
+  router.get('/', authenticate, async (req, res) => {
     try {
       const result = await pool.query('SELECT * FROM usuario');
       res.json(result.rows);
@@ -14,10 +24,10 @@ module.exports = (pool) => {
   });
 
   //Rota para autentificar um usuario
-  router.get('/:email/:senha', async (req, res) => {
+  router.get('/:email/:senha', authenticate, async (req, res) => {
     const { email, senha } = req.params;
     try {
-      const result = await pool.query('SELECT * FROM usuario WHERE email = $1 and senha = $2' , [email, senha]);
+      const result = await pool.query('SELECT * FROM usuario WHERE email = $1 and senha = $2', [email, senha]);
       if (result.rows.length === 0) {
         res.status(404).send('Usuário não encontrado');
       } else {
@@ -30,7 +40,7 @@ module.exports = (pool) => {
   });
 
   // Rota para buscar um usuário por email
-  router.get('/:email', async (req, res) => {
+  router.get('/:email', authenticate, async (req, res) => {
     const { email } = req.params;
     try {
       const result = await pool.query('SELECT * FROM usuario WHERE email = $1', [email]);
@@ -46,9 +56,9 @@ module.exports = (pool) => {
   });
 
   // Rota para criar um usuário
-  router.post('/', (req, res) => {
-    const { email, nome_completo, senha, telefone, departamento, universidade} = req.body;
-  
+  router.post('/', authenticate, (req, res) => {
+    const { email, nome_completo, senha, telefone, departamento, universidade } = req.body;
+
     // Realizar a inserção no banco de dados
     pool.query(
       'INSERT INTO usuario (email, nome_completo, senha, telefone, departamento, universidade) VALUES ($1, $2, $3, $4, $5, $6)',
@@ -63,40 +73,37 @@ module.exports = (pool) => {
     );
   });
 
-  router.put('/:email', (req, res) => {
+  router.put('/:email', authenticate, (req, res) => {
     const { nome_completo, senha, telefone, departamento, universidade, descricao, link_imagem } = req.body;
     const { email } = req.params;
-  
-  
-    pool.query('UPDATE usuario SET nome_completo = $1, senha = $2, telefone = $3, departamento = $4, universidade = $5, descricao = $6, link_imagem = $7 WHERE email = $8', 
-      [nome_completo, senha, telefone, departamento, universidade, descricao, link_imagem, email], 
-      (error, results) => {
-        if (error) {
-          throw error;
-        }
-        res.status(200).send(`Usuário atualizado com sucesso!`);
-      });
-  });
-  
-  
 
-  router.delete('/:email', (req, res) => {
-    const email = req.params.email;
-    const query = 'DELETE FROM usuario WHERE email=$1';
-  
-    pool.query(query, [email], (error, results) => {
+
+    pool.query('UPDATE usuario SET nome_completo = $1, senha = $2, telefone = $3, departamento = $4, universidade = $5, descricao = $6, link_imagem = $7 WHERE email = $8',
+      [nome_completo, senha, telefone, departamento, universidade, descricao, link_imagem, email],
+      (error, result) => {
+        if (error) {
+          res.status(500).send(error.message);
+        } else if (result.rowCount === 0) {
+          res.status(404).send('Usuário não encontrado');
+        } else {
+          res.status(200).send('Usuário atualizado com sucesso!');
+        }
+      }
+    );
+  });
+
+  router.delete('/:email', authenticate, (req, res) => {
+    const { email } = req.params;
+    pool.query('DELETE FROM usuario WHERE email = $1', [email], (error, result) => {
       if (error) {
-        console.error(error);
-        res.status(500).send('Erro ao excluir usuário.');
+        res.status(500).send(error.message);
+      } else if (result.rowCount === 0) {
+        res.status(404).send('Usuário não encontrado');
       } else {
-        res.status(200).send(`Usuário ${email} excluído com sucesso.`);
+        res.status(200).send('Usuário excluído com sucesso!');
       }
     });
   });
-  
-
-
-
 
   return router;
-};
+};    
