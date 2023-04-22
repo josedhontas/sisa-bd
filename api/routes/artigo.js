6a3dd0a4ab90d5cdaa7412ec76ee28b4dd3a4fd7
josedhonatas <dhonatas@academico.ufs.br>
2023-04-22 08:17:17 -0300

    envio_grande

--- api/routes/artigo.js
+++ api/routes/artigo.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 
 module.exports = (pool) => {
 
@@ -15,10 +16,46 @@ module.exports = (pool) => {
   });
 
 
-  router.post('/', (req, res) => {
-    const { id_revista, email_revisor, palavras_chaves, nome_artigo, link_artigo, msg_revisor, resumo } = req.body;
-    const query = 'INSERT INTO artigo (id_revista, email_revisor, palavras_chaves, nome_artigo, link_artigo, msg_revisor, resumo) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id_artigo';
-    const values = [id_revista, email_revisor, palavras_chaves, nome_artigo, link_artigo, msg_revisor, resumo];
+  router.get('/:id', async (req, res) => {
+    try {
+      const { id } = req.params;
+      const result = await pool.query('SELECT artigo FROM artigo WHERE id_artigo = $1', [id]);
+  
+      if (result.rows.length === 0) {
+        return res.status(404).json({ message: 'Artigo não encontrado' });
+      }
+  
+      const artigo = result.rows[0];
+  
+      // Define o tipo do conteúdo como pdf
+      res.setHeader('Content-Type', 'application/pdf');
+  
+      // Envia o conteúdo do artigo, que deve ser o arquivo PDF armazenado diretamente no banco de dados
+      res.send(artigo.artigo);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Erro ao buscar artigo' });
+    }
+  });
+  
+
+
+  const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+      cb(null, 'uploads/');
+    },
+    filename: function(req, file, cb) {
+      cb(null, file.originalname);
+    }
+  });
+  
+  const upload = multer({ storage: storage });
+  
+  router.post('/', upload.single('artigo'), (req, res) => {
+    const { id_revista, email_revisor, palavras_chaves, nome_artigo, msg_revisor, resumo } = req.body;
+    const artigo = req.file.path;
+    const query = 'INSERT INTO artigo (id_revista, email_revisor, palavras_chaves, nome_artigo, artigo, msg_revisor, resumo) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id_artigo';
+    const values = [id_revista, email_revisor, palavras_chaves, nome_artigo, artigo, msg_revisor, resumo];
     pool.query(query, values, (error, results) => {
       if (error) {
         console.error(error);
@@ -30,9 +67,9 @@ module.exports = (pool) => {
     });
   });
 
-  router.put('/artigo/:id', (req, res) => {
+  router.put('/:id', (req, res) => {
     const id = req.params.id;
-    const { id_revista, email_revisor, palavras_chaves, nome_artigo, link_artigo, msg_revisor, resumo } = req.body;
+    const { id_revista, email_revisor, palavras_chaves, nome_artigo,  msg_revisor, resumo } = req.body;
   
     // Verificar se o artigo com o id fornecido existe no banco de dados
     db.query('SELECT * FROM artigo WHERE id_artigo = $1', [id], (err, result) => {
@@ -45,8 +82,8 @@ module.exports = (pool) => {
       }
   
       // Atualizar o artigo no banco de dados
-      db.query('UPDATE artigo SET id_revista = $1, email_revisor = $2, palavras_chaves = $3, nome_artigo = $4, link_artigo = $5, msg_revisor = $6, resumo = $7 WHERE id_artigo = $8',
-        [id_revista, email_revisor, palavras_chaves, nome_artigo, link_artigo, msg_revisor, resumo, id], (err, result) => {
+      db.query('UPDATE artigo SET id_revista = $1, email_revisor = $2, palavras_chaves = $3, nome_artigo = $4, artigo = $5, msg_revisor = $6, resumo = $7 WHERE id_artigo = $8',
+        [id_revista, email_revisor, palavras_chaves, nome_artigo, artigo, msg_revisor, resumo, id], (err, result) => {
           if (err) {
             throw err;
           }
